e6a6207a548a1543843540e73e4e818f24783eef
alyaanasif12 <asifaly@sheridancollege.ca>
2019-05-21 13:41:29 -0400

    testing clone

---
+++ src/testingclone/TestingClone.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testingclone;
+
+/**
+ *
+ * @author 16475
+ */
+public class TestingClone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Test Clone");
+        // TODO code application logic here
+    }
+    
+}
